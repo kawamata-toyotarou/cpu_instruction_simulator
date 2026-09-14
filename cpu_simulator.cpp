5b2580204af1fd83cd22ed6d7ef663eea2688832
toydayo <kawamataro1209@gmail.com>
2026-09-15 00:18:01 +0900

    STOの実装

--- cpu_simulator.cpp
+++ cpu_simulator.cpp
@@ -40,25 +40,45 @@ int main(void) {
         stringstream ss(instruction);
 
         string command;
-        int value;
+        string value;
         ss  >> command >> value;
 
         cout << "PC = " << PC << endl;
         cout << "命令 = " << instruction << endl;
+        cout << "値 = " << value << endl;
 
         //LDI命令の場合
-        if (instruction == "LDI") {
-            A = value;
+        if (command == "LDI") {
+            A = stoi(value);
+        }
+
+        //STO命令の場合
+        else if (command == "STO") {
+            
+            if (value == "a") {
+                a = A;
+            }
+            else if (value == "b") {
+                b = A;
+            }
+            else if (value == "c") {
+                c = A;
+            }
         }
 
         PC++;
 
-        if (PC > program.size()) {
+        if (PC >= program.size()) {
             break;
         }
     }
 
-    cout << "A= " << A << endl;
+    cout << endl;
+
+    cout << "A = " << A << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
 
     return 0;
 
